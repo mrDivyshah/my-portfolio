7d4d83194f91df33959c7bfacf9c47c62ba52c64
mrDivyshah <105691678+mrDivyshah@users.noreply.github.com>
2024-04-11 12:35:57 +0530

    project proper run

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'))
 app.get('/', (req, res) => {
-  res.render('index');
+    fs.readFile('data/data,json', 'utf8', (error, data) =>{
+        if (error) {
+            console.error(error);
+            return;
+        }
+        else {
+            console.log(data);
+            const jsonData = JSON.parse(data);
+            res.render('index', { data: jsonData });
+        }
+    } );
 });
 
 app.listen(3000, () => {
