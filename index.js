a4a70521250781add6bc7557989a5167506e6fbb
mrDivyshah <105691678+mrDivyshah@users.noreply.github.com>
2024-04-11 13:08:37 +0530

    change data.json path

--- index.js
+++ index.js
@@ -2,25 +2,29 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const path = require('path');
-const dataPath = path.join(__dirname, 'data', 'data.json');
-
+const dataPath = path.join(__dirname, './data', 'data.json');
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'))
+
 app.get('/', (req, res) => {
-    fs.readFile(dataPath, 'utf8', (error, data) =>{
+    fs.readFile(dataPath, 'utf8', (error, data) => {
         if (error) {
-            console.error(error);
+            console.error('Error reading data file:', error);
+            res.status(500).send('Internal Server Error');
             return;
         }
-        else {
-            console.log(data);
+        try {
             const jsonData = JSON.parse(data);
             res.render('index', { data: jsonData });
+        } catch (parseError) {
+            console.error('Error parsing JSON:', parseError);
+            res.status(500).send('Internal Server Error');
         }
-    } );
+    });
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
